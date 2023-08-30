83894dfba3a5f18f0e279a24a040abcdb3d21efa
Izz Hafeez <izzhafeez@gmail.com>
2023-08-30 23:06:58 +0800

    Edit ./src/person/person.rs

--- src/person/person.rs
+++ src/person/person.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+use std::sync::Arc;
 use serde_derive::{Deserialize, Serialize};
 use crate::location::station::Station;
 use crate::person::person_basic_information::PersonBasicInformation;
@@ -9,10 +11,13 @@ pub struct Person {
 }
 
 impl Person {
-    pub fn get() -> Vec<Person> {
+    pub fn get(station_map: HashMap<String, Station>) -> Vec<Person> {
         let pbi_vec: Vec<PersonBasicInformation> = PersonBasicInformation::get();
-        pbi_vec
-            .into_iter()
-            .map(|pbi| Person {})
+        for pbi in pbi_vec.into_iter() {
+            let name: String = pbi.name;
+            let station_name: String = pbi.station_name;
+            let station: Station = *station_map.get(&station_name)?.clone();
+
+        }
     }
 }
